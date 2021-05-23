d36b5e394bcab13fa79248bb3e4d0ffc3b0fbb75
Supervinh <81265673+Supervinh@users.noreply.github.com>
2021-05-23 08:51:19 +0200

    Update activation.py

--- Final V3/activation.py
+++ Final V3/activation.py
@@ -3,12 +3,12 @@ import classe
 import command
 import event
 import gif
-import animaux
+import dessins
 import morpion
 
 bot.add_cog(classe.Role(bot))
 bot.add_cog(command.Utilitaires(bot))
 bot.add_cog(command.Fun(bot))
-bot.add_cog(animaux.Dessins(bot))
+bot.add_cog(dessins.Dessins(bot))
 
 bot.run("Token")
